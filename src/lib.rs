6317ecb928c5d562a060a4a3d457a4ad2941f65d
Alwin <alwin@conshax.com>
2023-03-02 12:06:58 +0100

    simplify keysend

--- src/lib.rs
+++ src/lib.rs
@@ -58,29 +58,24 @@ impl Client {
         })
     }
 
-    pub async fn send_keysend(&self, dest: String, amt_msat: u64, mut dest_custom_records: HashMap<u64, String>, outgoing_chan_id: Option<u64>) -> Result<[u8; 32]> {
-        if dest.len() != 44 {
-            return Err(Error::InvalidParameter(String::from("invalid dest length, needs to be 44")));
-        }
+    pub async fn send_keysend(&self, dest_hex: &str, amt_msat: u64, mut dest_custom_records_slice: HashMap<u64, &[u8]>, outgoing_chan_id: Option<u64>) -> Result<[u8; 32]> {
 
         let mut pre_image = [0u8; 32];
-        getrandom::getrandom(&mut pre_image)?;
-
-        let pre_image_b64 = base64::engine::general_purpose::STANDARD.encode(&pre_image);
-        dest_custom_records.insert(5482373484, pre_image_b64);
+        getrandom::getrandom(&mut pre_image)?;        
+        dest_custom_records_slice.insert(5482373484, &pre_image);
 
         let mut hasher = Sha256::new();
         hasher.update(pre_image);
-        
-        let payment_hash = hasher.finalize().to_vec();
-        dbg!(hex::encode(&payment_hash));
-        let payment_hash = base64::engine::general_purpose::STANDARD.encode(payment_hash);
+        let payment_hash = slice_to_b64(hasher.finalize().as_slice());
+
+        let dest_b64 = hex_to_base64(dest_hex)?;
+        let dest_custom_records_b64 = dest_custom_records_slice.into_iter().map(|(k, v)| (k, slice_to_b64(v))).collect();
 
         let request_body = SendRequestBody {
-            dest,
+            dest: dest_b64,
             amt_msat,
             payment_hash,
-            dest_custom_records,
+            dest_custom_records: dest_custom_records_b64,
             outgoing_chan_id,
         };
 
@@ -103,9 +98,13 @@ impl Client {
     }
 }
 
-pub fn hex_to_base64(hex_str: &str) -> Result<String> {
+fn hex_to_base64(hex_str: &str) -> Result<String> {
     let bytes = hex::decode(hex_str)?;
     Ok(base64::engine::general_purpose::STANDARD.encode(bytes.as_slice()))
+}
+
+fn slice_to_b64(slice: &[u8]) -> String {
+    base64::engine::general_purpose::STANDARD.encode(slice)
 }
 
 //TODO implement missing https://github.com/lightningnetwork/lnd/blob/d44823f6e8580d2fa5f193a5382be351610f7c2b/lnrpc/lightning.proto#L769
@@ -128,28 +127,22 @@ struct SendResponseBody {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use hex;
 
     #[tokio::test]
     async fn test_send_keysend() {
         let node_url = std::env::var("CONSHAX_NODE_URL").expect("Missing CONSHAX_NODE_URL env");
-        let macaroon = std::env::var("CONSHAX_NODE_MACAROON").expect("Missing CONSHAX_NODE_MACAROON env");
+        let macaroon = std::env::var("CONSHAX_NODE_MACAROON").expect("Missing CONSHAX_NODE_MACAROON env");        
+        let destination = std::env::var("ALBY_NODE_ADDRESS").expect("Missing ALBY_NODE_ADDRESS env");
+        let custom_value = std::env::var("ALBY_TEST_USER_CUSTOM_VALUE"). expect("Missing ALBY_TEST_USER_CUSTOM_VALUE env");
 
         let client = Client::new(node_url, &macaroon);
         assert!(client.is_ok());
         let client = client.unwrap();
 
-        let destination = std::env::var("ALBY_NODE_ADDRESS").expect("Missing ALBY_NODE_ADDRESS env");
-        let dest_hex = hex::decode(destination).expect("Could not decode destination to hex");
-        let dest_b64 = base64::engine::general_purpose::STANDARD.encode(dest_hex.as_slice());
-    
 
-        let custom_value = std::env::var("ALBY_TEST_USER_CUSTOM_VALUE"). expect("Missing ALBY_TEST_USER_CUSTOM_VALUE env");
-        let custom_value_b64 = base64::engine::general_purpose::STANDARD.encode(custom_value);
+        let custom_records = HashMap::from_iter([(696969, custom_value.as_bytes())]);
 
-        let custom_records = HashMap::from_iter([(696969, custom_value_b64)]);
-
-        let res = client.send_keysend(dest_b64, 1000, custom_records, None).await;
+        let res = client.send_keysend(&destination, 1000, custom_records, None).await;
 
         if matches!(res, Ok(_)) {
             assert!(true)            
